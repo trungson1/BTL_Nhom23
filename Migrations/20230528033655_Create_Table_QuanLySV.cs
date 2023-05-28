9ea6c7e36e0dfc094f68202eabd4c93aae273d37
trungson1 <luongtrungson01@gmail.com>
2023-05-28 10:45:16 +0700

    first commit

---
+++ Migrations/20230528033655_Create_Table_QuanLySV.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QL_SinhVien.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_Table_QuanLySV : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SDT",
+                table: "QuanLySV",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SDT",
+                table: "QuanLySV");
+        }
+    }
+}
